9d24648bac23db7a68fe3da3a2a1486c23114f92
Oane Hoeksma <oane.hoeksma@outlook.com>
2026-08-28 10:42:43 +0200

    Les 8 toegevoegd

---
+++ algemene_functies.py
@@ -0,0 +1,17 @@
+def mijn_functie_1(x):
+    return x ** 2
+
+print(mijn_functie_1(2))   # 4
+print(mijn_functie_1(4))   # 16
+print(mijn_functie_1(10))  # 100
+print(mijn_functie_1(12))  # 144
+
+def mijn_functie_2(a, b):
+    return [a + b, a - b, a * b, a / b]
+
+
+print(mijn_functie_2(12, 3))
+print(mijn_functie_2(12, 2))
+print(mijn_functie_2(10, 5))
+print(mijn_functie_2(100, 20))
+
