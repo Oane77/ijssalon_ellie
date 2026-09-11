3e4a462df3db102bbc661f1104d6a50c271032f7
Oane Hoeksma <oane.hoeksma@outlook.com>
2026-09-11 09:59:18 +0200

    CSV-functionaliteit toegevoegd

---
+++ helper.py
@@ -0,0 +1,11 @@
+tekst = "header"
+lengte = len(tekst) + 4
+
+print()  
+print(lengte * "*")
+print(f"* {tekst} *")
+print(lengte * "*")
+print()
+
+def som(dictionary):
+    return sum(dictionary.values())
